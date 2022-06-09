84ddd21829a37eafaf71a69ecf8addda3e4b7f6e
Mistralys <s.mordziol@gmail.com>
2022-06-09 15:20:00 +0200

    Increased level to `9`.

--- docs/run-phpstan.bat
+++ docs/run-phpstan.bat
@@ -1,13 +1,15 @@
 @echo off
 
-cls 
+cls
+
+set Level=9
 
 echo -------------------------------------------------------
-echo RUNNING PHPSTAN ANALYSIS
+echo RUNNING PHPSTAN ANALYSIS @ %Level%
 echo -------------------------------------------------------
 
 echo.
 
-call ../vendor/bin/phpstan analyse -c ./config/phpstan.neon -l 7 > phpstan/output.txt
+call ../vendor/bin/phpstan analyse -c ./config/phpstan.neon -l %Level% > phpstan/output.txt
 
 start "" "phpstan/output.txt"
